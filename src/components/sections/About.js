69ef483788ee802f8af8e5f569310a81d4fb6015
tomiandic <tomi.andic@gmail.com>
2021-11-03 17:32:15 +0100

    changes

---
+++ src/components/sections/About.js
@@ -0,0 +1,9 @@
+import React, { useEffect, useState, useRef } from "react";
+import { Link } from "gatsby";
+import { gsap, Power3, Sine } from "gsap";
+import * as classes from "../../styles/about.module.css";
+
+const About = (props) => {
+  return <section></section>;
+};
+export default About;
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React, { useEffect, useRef, useState } from "react";
 import * as classes from "../styles/landing.module.css";
 import Loader from "../components/Loader";
 import { gsap, Power3, Sine } from "gsap";
+import About from "../components/sections/About";
 
 import Header from "../components/Header";
 
@@ -77,97 +78,102 @@ const IndexPage = () => {
   }
 
   return (
-    <section className={classes.landing}>
-      <Header />
-      <Loader landingVideoRef={landingVideo} pageIsLoaded={pageIsLoaded} />
+    <>
+      <section className={classes.landing}>
+        <Header />
+        <Loader landingVideoRef={landingVideo} pageIsLoaded={pageIsLoaded} />
 
-      <div className={classes.backdropGradient} />
-      <video
-        className={classes.videoBackdrop}
-        src="/1.mp4"
-        ref={landingVideo}
-        muted
-        playsInline
-        loop
-        autoPlay
-      />
-      <div className={classes.landingContent} data-animation="landing-loader">
-        <h1>
-          <div className={classes.line}>
-            <span ref={(el) => addToRefs(el, landingTitle)}>2022 Tickets</span>
-          </div>
-          <div className={classes.line}>
-            <span ref={(el) => addToRefs(el, landingTitle)}>
-              {" "}
-              Now Available!
-            </span>
-          </div>
-        </h1>
-        <svg className={classes.svgWave} viewBox="0 0 25.3 4">
-          <path
-            d="M4.85,24c2.24,0,2.24,2,4.47,2s2.24-2,4.48-2,2.24,2,4.48,2,2.24-2,4.47-2,2.24,2,4.48,2,2.24-2,4.47-2,2.24,2,4.48,2,2.25-2,4.49-2,2.24,2,4.48,2"
-            transform="translate(-4.85 -23)"
-            fill="none"
-          />
-          <path
-            ref={landingSvg}
-            d="M4.85,24c2.24,0,2.24,2,4.47,2s2.24-2,4.48-2,2.24,2,4.48,2,2.24-2,4.47-2,2.24,2,4.48,2,2.24-2,4.47-2,2.24,2,4.48,2,2.25-2,4.49-2,2.24,2,4.48,2"
-            transform="translate(-4.85 -23)"
-            fill="none"
-            stroke="#00e8fc"
-            strokeMiterlimit="20"
-            strokeWidth="2"
-          />
-        </svg>
-        <p className={classes.landingParagraph}>
-          <div className={classes.line}>
-            <span ref={(el) => addToRefs(el, landingTitle)}>
-              Order your boat party ticket on time
-            </span>
-          </div>
-          <div className={classes.line}>
-            <span ref={(el) => addToRefs(el, landingTitle)}>
-              to get the early bird prices,
-            </span>
-          </div>
-          <div className={classes.line}>
-            <span ref={(el) => addToRefs(el, landingTitle)}>
-              and much more.
-            </span>
-          </div>
-        </p>
-
-        <button ref={landingButton} className={classes.button}>
-          ORDER TICKETS{" "}
-          <svg
-            style={{ marginLeft: 10 }}
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-          >
-            <polygon points="11.293 4.707 17.586 11 4 11 4 13 17.586 13 11.293 19.293 12.707 20.707 21.414 12 12.707 3.293 11.293 4.707" />
+        <div className={classes.backdropGradient} />
+        <video
+          className={classes.videoBackdrop}
+          src="/1.mp4"
+          ref={landingVideo}
+          muted
+          playsInline
+          loop
+          autoPlay
+        />
+        <div className={classes.landingContent} data-animation="landing-loader">
+          <h1>
+            <div className={classes.line}>
+              <span ref={(el) => addToRefs(el, landingTitle)}>
+                2022 Tickets
+              </span>
+            </div>
+            <div className={classes.line}>
+              <span ref={(el) => addToRefs(el, landingTitle)}>
+                {" "}
+                Now Available!
+              </span>
+            </div>
+          </h1>
+          <svg className={classes.svgWave} viewBox="0 0 25.3 4">
+            <path
+              d="M4.85,24c2.24,0,2.24,2,4.47,2s2.24-2,4.48-2,2.24,2,4.48,2,2.24-2,4.47-2,2.24,2,4.48,2,2.24-2,4.47-2,2.24,2,4.48,2,2.25-2,4.49-2,2.24,2,4.48,2"
+              transform="translate(-4.85 -23)"
+              fill="none"
+            />
+            <path
+              ref={landingSvg}
+              d="M4.85,24c2.24,0,2.24,2,4.47,2s2.24-2,4.48-2,2.24,2,4.48,2,2.24-2,4.47-2,2.24,2,4.48,2,2.24-2,4.47-2,2.24,2,4.48,2,2.25-2,4.49-2,2.24,2,4.48,2"
+              transform="translate(-4.85 -23)"
+              fill="none"
+              stroke="#00e8fc"
+              strokeMiterlimit="20"
+              strokeWidth="2"
+            />
           </svg>
-        </button>
-        <div className={classes.socialMediaContainer}>
-          <a
-            target="_blank"
-            rel="noopener"
-            href="https://www.instagram.com/instaboom.pula/?hl=hr"
-            ref={(el) => addToRefs(el, socialMedia)}
-          >
-            <img src="icons/instagram-white.svg" />
-          </a>
-          <a
-            target="_blank"
-            rel="noopener"
-            href="https://www.facebook.com/BOOMeventsPula/"
-            ref={(el) => addToRefs(el, socialMedia)}
-          >
-            <img src="icons/facebook-white.svg" />
-          </a>
+          <p className={classes.landingParagraph}>
+            <div className={classes.line}>
+              <span ref={(el) => addToRefs(el, landingTitle)}>
+                Order your boat party ticket on time
+              </span>
+            </div>
+            <div className={classes.line}>
+              <span ref={(el) => addToRefs(el, landingTitle)}>
+                to get the early bird prices,
+              </span>
+            </div>
+            <div className={classes.line}>
+              <span ref={(el) => addToRefs(el, landingTitle)}>
+                and much more.
+              </span>
+            </div>
+          </p>
+
+          <button ref={landingButton} className={classes.button}>
+            ORDER TICKETS{" "}
+            <svg
+              style={{ marginLeft: 10 }}
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+            >
+              <polygon points="11.293 4.707 17.586 11 4 11 4 13 17.586 13 11.293 19.293 12.707 20.707 21.414 12 12.707 3.293 11.293 4.707" />
+            </svg>
+          </button>
+          <div className={classes.socialMediaContainer}>
+            <a
+              target="_blank"
+              rel="noopener"
+              href="https://www.instagram.com/instaboom.pula/?hl=hr"
+              ref={(el) => addToRefs(el, socialMedia)}
+            >
+              <img src="icons/instagram-white.svg" />
+            </a>
+            <a
+              target="_blank"
+              rel="noopener"
+              href="https://www.facebook.com/BOOMeventsPula/"
+              ref={(el) => addToRefs(el, socialMedia)}
+            >
+              <img src="icons/facebook-white.svg" />
+            </a>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+      <About />
+    </>
   );
 };
 
