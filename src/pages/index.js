168aba171d1bc9a1da950c5cb735b410b2fe3f84
tomiandic <tomi.andic@gmail.com>
2021-11-13 11:04:01 +0100

    Changed slider

--- src/pages/index.js
+++ src/pages/index.js
@@ -217,6 +217,7 @@ const IndexPage = () => {
                 <polygon points="11.293 4.707 17.586 11 4 11 4 13 17.586 13 11.293 19.293 12.707 20.707 21.414 12 12.707 3.293 11.293 4.707" />
               </svg>
             </SwiperSlide>
+            <SwiperSlide></SwiperSlide>
           </Swiper>
         </div>
       </section>
