a386b68f2d1d739e2f79e43c4d676324ff13fb7a
tomiandic <tomi.andic@gmail.com>
2022-02-13 13:19:56 +0100

    added itinerary timeline

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,16 +1,10 @@
 module.exports = {
-    siteMetadata: {
-        siteUrl: `https://www.boomevents.hr`,
-    },
-    plugins: [
-        {
-            resolve: `gatsby-source-instagram`,
-            options: {
-              username: `11629345558`,
-            },
-        },
-        `gatsby-plugin-image`,
-        `gatsby-plugin-sharp`,
-        `gatsby-transformer-sharp`, // Needed for dynamic images
-    ]
-}
+  siteMetadata: {
+    siteUrl: `https://www.boomevents.hr`,
+  },
+  plugins: [
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`, // Needed for dynamic images
+  ],
+};
