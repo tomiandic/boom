8e062162b27173c09560eb7439b21ce1eb171f60
Tomislav Andic <tandjic@iolap.com>
2022-03-01 21:38:01 +0100

    added booking timer

--- src/pages/event/booking.js
+++ src/pages/event/booking.js
@@ -3,17 +3,31 @@ import * as classes from "../../styles/booking.module.css";
 import { StaticImage } from "gatsby-plugin-image";
 import BookingStep from "../../components/BookingStep";
 import StepWizard from "react-step-wizard";
+import Header from "../../components/Header";
+import Countdown from "react-countdown";
+
 
 
 const Booking = () => {
   return (
     <section className={classes.bookingSection}>
-      <StepWizard>
-          <BookingStep />
-          <BookingStep />
-          <BookingStep />
-          <BookingStep />
-      </StepWizard>
+      <Header/>
+      <div>
+        <div className={classes.eventHolder}>
+          <StaticImage className={classes.bookingEventImage} src="../images/b-top.jpg" />
+          <div>
+            <h6>Party Boom Boat Party</h6>
+            <p><span>Jan 6. </span> <span> Pula </span><span> 18:00</span></p>
+          </div>
+          <Countdown date={Date.now() + 10000}  />
+        </div>
+        <StepWizard>
+            <BookingStep />
+            <BookingStep />
+            <BookingStep />
+            <BookingStep />
+        </StepWizard>
+      </div>
     </section>
   );
 };
