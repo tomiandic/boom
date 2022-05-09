6a3af73c835d003d74bd4bd64e6602f1893f26e3
Tomislav Andic <tandjic@iolap.com>
2022-05-09 12:09:25 +0200

    Navbar modified

--- src/components/Header.js
+++ src/components/Header.js
@@ -15,6 +15,7 @@ const Header = () => {
   const navLinksDesktop = useRef([]);
   const socialMedia = useRef([]);
   const sidebarTL = useRef(gsap.timeline({ paused: true }));
+  const blackLogo = useRef();
 
   useEffect(() => {
     sidebarTL.current
@@ -72,9 +73,9 @@ const Header = () => {
         "-=1.2"
       );
 
-
+ 
     gsap.to(nav.current, {
-      background: "#ffffffb3",
+      background: "rgba(255, 255, 255, 0.9)",
       boxShadow: "0 0 30px -20px #97a6c7",
       ease: Power3.easeOut,
       scrollTrigger: {
@@ -96,6 +97,17 @@ const Header = () => {
       },
     });
 
+    gsap.to(blackLogo.current, {
+      opacity: 1,
+      ease: Power3.easeOut,
+      scrollTrigger: {
+        trigger: "#about",
+        scrub: 1,
+        start: "top bottom", //when the top of the trigger hits bottom of vw
+        end: "+=100%"
+      },
+    })
+
     
 
   }, []);
@@ -115,7 +127,8 @@ const Header = () => {
   return (
     <nav className={classes.headerContainer} ref={nav}>
       <Link to="/">
-        <img className={classes.logo} src="/boom_final_black.svg" />
+        <img className={classes.logo} src="/boom_all_white.svg" />
+        <img style={{opacity: 0}} ref={blackLogo} className={classes.logo} src="/boom_final_black.svg" />
       </Link>
       <div className={classes.menuInner} ref={mobileNav}>
         <div className={classes.navLinks}>
